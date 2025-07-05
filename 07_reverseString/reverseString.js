63415bf3896f36a6acc2e8b3d0a2602a09097271
l3130 <kmett32@gmail.com>
2025-07-05 11:40:12 +0100

    Completed 07_reverseString exercise with test

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    // Split the string into an array of characters, reverse the array, and join it back into a string
+    return str.split('').reverse().join('');
 };
 
 // Do not edit below this line
