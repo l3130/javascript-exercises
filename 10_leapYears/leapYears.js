f02813116dc21d3a27b142d8718433c0cb3205eb
l3130 <kmett32@gmail.com>
2025-07-07 16:13:27 +0100

    Completed 10_leapYear exercise with tests

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  // Check if the year is divisible by 4
+  if (year % 4 === 0) {
+    // If it is a century year, check if it is divisible by 400
+    if (year % 100 === 0) {
+      return year % 400 === 0; // Century years are leap years only if divisible by 400
+    }
+    return true; // Non-century years that are divisible by 4 are leap years
+  }
+  return false; // All other years are not leap years
 
 };
 
