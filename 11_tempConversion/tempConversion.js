09a81917595bfc7ced90cb9ec0f750e1034f926a
l3130 <kmett32@gmail.com>
2025-07-09 12:06:51 +0100

    Completed 11_tempConversion exercise with tests

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(n) {
+  if (typeof n !== 'number') {
+    throw new Error('Input must be a number');
+  }
+  return Math.round(((n - 32) * 5 / 9) * 10) / 10; // Round to 1 decimal place
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(n) {
+  if (typeof n !== 'number') {
+    throw new Error('Input must be a number');
+  }
+  return Math.round(((n * 9 / 5) + 32) * 10) / 10; // Round to 1 decimal place
+
 };
 
 // Do not edit below this line
