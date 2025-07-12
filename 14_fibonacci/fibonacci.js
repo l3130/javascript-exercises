a77774176fe1c8c89bb8c7d1bb2225f5b45cb50b
l3130 <kmett32@gmail.com>
2025-07-12 12:28:01 +0100

    Completed 14_fibonacci exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) {
+        throw new Error("Input cannot be a negative number");
+    }
+    if (n === 0) {
+        return 0;
+    }
+    if (n === 1) {
+        return 1;
+    }
+    
+    let a = 0, b = 1, temp;
+    for (let i = 2; i <= n; i++) {
+        temp = a + b;
+        a = b;
+        b = temp;
+    }
+    
+    return b;
 
 };
 
